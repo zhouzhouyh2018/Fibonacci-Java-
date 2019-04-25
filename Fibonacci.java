42b27be4a20081dbb100f7fb38b67cece249281c
Jane <44191210+zhouzhouyh2018@users.noreply.github.com>
2019-04-25 15:13:30 +0800

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Fibonacci{
+    public static BigInteger fo(int num){
+        BigInteger num1 = BigInteger.ONE;
+        BigInteger num2 = BigInteger.ONE;
+        BigInteger temp = BigInteger.ZERO;
+        for(int i = 1; i <= num; ++i){
+            if(i <= 2)
+            	num1 = BigInteger.ONE;
+            else{
+            	
+            	
+            	temp = num1.add(num2);
+            	num2 = num1;
+            	num1 = temp;
+            }
+        }
+        return num1;
+    }
+    public static void main(String[] args){
+    	System.out.println("Please enter a number:");
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNextInt()){
+            int num = scan.nextInt();
+            for(int i=1;i<=200;++i){
+            	System.out.println("Fibonacci.fo(" + i + ") == " + Fibonacci.fo(i));
+            }
+        }
+    }
+}
